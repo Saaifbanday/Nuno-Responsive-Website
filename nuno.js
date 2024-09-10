d36863e2a6c118ecb52447c6cfd0f9175888621a
Saaifbanday <saaifbanday136@gmail.com>
2024-09-10 22:50:18 +0530

    more to come

---
+++ nuno.js
@@ -0,0 +1,19 @@
+const open = document.querySelector('.open')
+const close = document.querySelector('.close')
+const responsive = document.querySelector('.responsive')
+
+open.addEventListener('click',()=>{
+     if(responsive.classList.contains('responsive')){
+        responsive.classList.remove('responsive')
+        responsive.style.display = "none"
+        // open.style.display = "none"
+    }
+    else{
+        responsive.classList.add('responsive')
+        responsive.style.display = "block"
+        close.style.display = "block"
+
+    } 
+  
+})
+   
